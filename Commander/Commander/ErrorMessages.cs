0db316f5a2f592c6e8a484875f192add5481a7d6
Arif Yayalar <ayayalar@mac.com>
2018-07-06 13:14:31 -0700

    Minor bug fixes and refactoring

--- Commander/Commander/ErrorMessages.cs
+++ Commander/Commander/ErrorMessages.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Commander
+﻿namespace Commander
 {
     public static class ErrorMessages
     {
         public const string OverrideWithNewInstance = "Model cannot be replaced with another instance.";
+        public const string HandleEventIsNotRegistered = "Event is not handled. Must call HandleEvent<T> in Init method.";
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 Commander is a small library combines command pattern, with DDD practices. I think it is best explained by an example.
 
+Download here [DotNetCommander](https://www.nuget.org/packages/DotNetCommander/)
+
+```
+PM> Install-Package DotNetCommander -Version 1.1.0
+```
+
 
 ### Example
 
